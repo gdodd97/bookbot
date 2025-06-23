68b8e7ce4a9e6772957e95ebaaaba56e58c09efd
gdodd97 <gdodd97@gmail.com>
2025-06-23 10:45:32 -0400

    added sys args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words,count_letters
 
 def get_book_text(filepath):
@@ -8,13 +9,18 @@ def get_book_text(filepath):
 
 
 def main():
-   print("============ BOOKBOT ============")
-   print("Analyzing book found at books/frankenstein.txt...")
-   print("----------- Word Count ----------")
-   num = count_words(get_book_text("./books/frankenstein.txt"))
-   letter_count = count_letters(num)
-   for letter, count in sorted(letter_count.items(), key=lambda x: x[1], reverse=True):
-        print(f"{letter}: {count}")
+    args = sys.argv
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        num = count_words(get_book_text(args[1]))
+        letter_count = count_letters(num)
+        for letter, count in sorted(letter_count.items(), key=lambda x: x[1], reverse=True):
+            print(f"{letter}: {count}")
 
 
 main()
